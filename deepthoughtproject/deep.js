75e8564f4d3f9fb4c880023c7dc8bc4926c76291
Ajay maroti dewalkar <110341655+ajaydewalkar12@users.noreply.github.com>
2022-12-18 23:37:08 +0530

    Add files via upload

---
+++ deepthoughtproject/deep.js
@@ -0,0 +1,10 @@
+
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "215px";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "120px";
+  document.getElementById("main").style.marginLeft= "130px";
+}
